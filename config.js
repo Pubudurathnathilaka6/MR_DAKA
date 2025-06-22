964f7dee7b36d3f631eab9d9427ca2999b65b540
Pubudurathnathilaka6 <Pubudurathnathilaka6@gmail.com>
2025-06-23 02:02:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
   OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
   PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://drive.google.com/file/d/11IWXN7FjbpwxYBBQyR2F9AOoz6REtxgh/view?usp=drivesdk",
 };
