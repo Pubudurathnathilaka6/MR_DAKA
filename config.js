42de08531aa19d789b7a181527c407c1682c7271
Pubudurathnathilaka6 <Pubudurathnathilaka6@gmail.com>
2025-06-23 02:17:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,4 +10,7 @@ module.exports = {
   OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://drive.google.com/file/d/11IWXN7FjbpwxYBBQyR2F9AOoz6REtxgh/view?usp=drivesdk",
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n🥷Made by DAKA XMD👑",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  MODE:process.env.MODE || "public",
 };
