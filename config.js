599f2317a7cb74d0b1379a264da0b2195ac433a2
Pubudurathnathilaka6 <Pubudurathnathilaka6@gmail.com>
2025-06-23 01:36:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,5 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:hDloAjktLCcVwUZRgYZBcVFOYJSejbHs@caboose.proxy.rlwy.net:38647",
   OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
 };
