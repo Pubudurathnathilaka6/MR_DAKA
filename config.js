8b725db3934576705c8848bc6a8321e4c7cd86fb
Pubudurathnathilaka6 <Pubudurathnathilaka6@gmail.com>
2025-06-23 03:51:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,5 +12,6 @@ module.exports = {
   ALIVE_IMG: process.env.ALIVE_IMG || "https://drive.google.com/file/d/11IWXN7FjbpwxYBBQyR2F9AOoz6REtxgh/view?usp=drivesdk",
   ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n🥷Made by DAKA XMD👑",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-  MODE:process.env.MODE || "public",
+  MODE: process.env.MODE || "public",
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyAGGp4aNSDduYAv_GRNvzH2bUNatdhtcG4",
 };
